274d6ca64944f422cd1d868e6158fa70cac18f37
Rene Ciak <reciak@student.42vienna.com>
2025-02-09 19:17:30 +0100

    C04, ex03: ft_atoi  compiles and tests are ok for not too big numbers;
    The relevant integer range -2^31 ...  2^31 - 1 seems to work though

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,11 +6,14 @@
 /*   By: Rene Ciak <rciakAT42Vienna@web.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 11:47:44 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/09 18:53:49 by Rene Ciak        ###   ########.fr       */
+/*   Updated: 2025/02/09 19:15:14 by Rene Ciak        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// REMARK: atoi is somewhat flawed: it returns 0 on error and does not set errno
+// REMARKS: 
+// a) for large digitstrings atoi returns -1, whereas my ft_atoi returns
+//    in general somethings else (even more garbage than  atoi )
+// b) atoi is somewhat flawed: it returns 0 on error and does not set errno
 // --> Return value 0 has two potential meanings
 //     a) Everything worked well and 0 is just a regular return value, e.g by 
 //          atoi("-0") 
@@ -18,10 +21,6 @@
 // Good luck distinguishing between a) and b) when errno is not set by atoi ...
 // --> Looking at  atol  might be worthwhile
 #include <stdlib.h>       // Def. NULL
-
-
-
-
 
 int	ft_atoi(char *s)
 {
@@ -31,16 +30,16 @@ int	ft_atoi(char *s)
 	int 		sign_factor;
 	long long	nbr;
 
-
-
-
-
-	st_skip_white_space(&s);
+	if (s == NULL)
+		return (0);
+	while (*s == ' ' || *s == '\n' || *s == '\t' 
+		|| *s == '\v' || *s == '\f' || *s == '\r' )
+		s++;
 	if (*s == '\0' || !(*s == '+' || *s == '-' || ('0' <= *s && *s <= '9')))
 		return 0;
 	sign_factor = 1;
 	if (*s == '-')
-		sign_factor == -1;
+		sign_factor = -1;
 	if (*s == '+' || *s == '-')
 		s++;
 	if (!('0' <= *s && *s <= '9'))
