4ccd5aa2d26783ab9b295a5749b1aca3f100dc13
Rene Ciak <reciak@student.42vienna.com>
2025-02-13 20:22:47 +0100

    C01, ex00: Norminetted

--- tests_C_projects/C01_ex00.c
+++ tests_C_projects/C01_ex00.c
@@ -6,17 +6,17 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 20:15:34 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/13 20:16:03 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/13 20:20:33 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void ft_ft(int *nbr);
+void	ft_ft(int *nbr);
 
-int main (void)
+int	main(void)
 {
-	int n;
+	int	n;
 
 	n = -3;
 	printf("Before calling ft_ft the value of n is:|%d|\n", n);
