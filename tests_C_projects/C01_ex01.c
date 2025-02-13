b30c9c1c20476dc573ec01e5b7e2d469b6586814
Rene Ciak <reciak@student.42vienna.com>
2025-02-13 20:50:09 +0100

    C01, ex01: norminetted except main for testing, see
    tests_C_projects/C01_ex01.c

--- tests_C_projects/C01_ex01.c
+++ tests_C_projects/C01_ex01.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 20:26:00 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/13 20:41:15 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/13 20:45:25 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@ void	ft_ultimate_ft(int *********nbr);
 int	main(void)
 {
 	int	n;
-
-	int *p1;
-	int **p2;
-	int ***p3;
-	int ****p4;
-	int *****p5;
-	int ******p6;
-	int *******p7;
-	int ********p8;
-	int *********p9;
+	int	*p1;
+	int	**p2;
+	int	***p3;
+	int	****p4;
+	int	*****p5;
+	int	******p6;
+	int	*******p7;
+	int	********p8;
+	int	*********p9;
 
 	n = 7;
 	p1 = &n;
@@ -38,7 +37,6 @@ int	main(void)
 	p7 = &p6;
 	p8 = &p7;
 	p9 = &p8;
-
 	printf("Before calling ft_ultimate_ft the value of n is:|%d|\n", n);
 	ft_ultimate_ft(p9);
 	printf("After calling ft_ultimate_ft the value of n is: |%d|\n", n);
