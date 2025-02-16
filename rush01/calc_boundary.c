8738cf2b96c07a49c44fe5d960a014b62e150465
Rene Ciak <reciak@student.42vienna.com>
2025-02-16 13:21:58 +0100

    Rush01: Implemented  calc_boundary  and  calc_boundary_helpers.c

--- rush01/calc_boundary.c
+++ rush01/calc_boundary.c
@@ -6,11 +6,20 @@
 /*   By: Rene Ciak <rciakAT42Vienna@web.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 12:23:55 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/16 12:26:52 by Rene Ciak        ###   ########.fr       */
+/*   Updated: 2025/02/16 13:07:43 by Rene Ciak        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		calc_boundary(char **boundary, char **c_permed_matrix)
+# include "rush01.h"
+
+int	calc_boundary(char **boundary, char **c_permed_matrix)
 {
-	
+	calc_upper_border(boundary[LOOK_FROM_NORTH], c_permed_matrix);
+	calc_down_border(boundary[LOOK_FROM_SOUTH], c_permed_matrix);
+	calc_left_border(boundary[LOOK_FROM_WEST], c_permed_matrix);
+	calc_right_border(boundary[LOOK_FROM_EAST], c_permed_matrix);
 }
+
+
+
+
