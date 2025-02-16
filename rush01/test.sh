86da1e8b9e6e446b975b74b3c963c953dec2c661
Rene Ciak <reciak@student.42vienna.com>
2025-02-16 20:43:11 +0100

    Rush01: Header change in main.c

--- rush01/test.sh
+++ rush01/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc -Wall -Wextra -Werror -g ./calc_boundary.c ./calc_boundary_helpers.c ./check_input.c ./find_solution.c ./initialise.c ./is_matching.c ./print_solution.c ./rush01.h ./new_main.c
+gcc -Wall -Wextra -Werror -g ./calc_boundary.c ./calc_boundary_helpers.c ./check_input.c ./find_solution.c ./initialise.c ./is_matching.c ./print_solution.c ./rush01.h ./main.c
 
 echo "------T1(Correct)-----"
 ./a.out "1 3 2 3 3 1 3 2 1 2 3 2 3 2 1 2" | cat -e
