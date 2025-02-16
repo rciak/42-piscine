fe04e6b2338883fe86474307b47f1b2fa964c53f
Rene Ciak <reciak@student.42vienna.com>
2025-02-16 23:00:52 +0100

    C01, ex05:  ft_putstr  written, not tested (nor normitted)

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 22:53:48 by reciak            #+#    #+#             */
+/*   Updated: 2025/02/16 23:00:21 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+static size_t	ft_strlen(char* str);
+
+void	ft_putstr(char *str)
+{
+	size_t	len;
+
+	if (str == NULL)
+		return;
+	len = my_strlen(str);
+	write(1, str, len);
+}
+
+static size_t	my_strlen(char* str)
+{
+	size_t	len;
+
+	len  = 0;
+	while (str[i])
+		len++;
+	return (len);
+}
