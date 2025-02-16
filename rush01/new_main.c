41c2ec9b916a0d8797a1d5ab30b8418f5db2becb
Rene Ciak <reciak@student.42vienna.com>
2025-02-16 09:57:14 +0100

    Rush01: Drafted:  new_main.c  and  rush01.h
    More detailed info
    potentially with line breaks

--- rush01/new_main.c
+++ rush01/new_main.c
@@ -6,26 +6,27 @@
 /*   By: Rene Ciak <rciakAT42Vienna@web.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 22:52:51 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/16 00:14:13 by Rene Ciak        ###   ########.fr       */
+/*   Updated: 2025/02/16 09:49:35 by Rene Ciak        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush01.h"
 
+static st_int	check_input_validity(int argc, char **argv);
+
 int main(int argc, char **argv)
 {
-	char		user_boundaries
+	char		user_boundaries[NUM_LOOKING_DIRECTIONS][SIZE];
 	char		solution[SIZE][SIZE];
-	char		the_matrix[4][SIZE][SIZE];
+	char		the_matrix[NUM_OF_BASIC_MATRICES][SIZE][SIZE];
 	t_gen_perms	gen_perms;
 
-	if (argc != 2)
-		return (-1);
-	if (!check_input_validity(argv[1]))
+	if (!check_input_validity(argc, argv[1]))
 	{
 		write(1, "Error\n", 6);
 		return (-1);
 	}
+	store_user_boundaries(user_boundaries, argv[1]);
 	clear_solution(solution);
 	set_the_matrix(the_matrix);
 	set_row_perm(gen_perms.row_perm);
@@ -38,4 +39,11 @@ int main(int argc, char **argv)
 	print_solution(solution);
 	write(1, "\n", 1);
 	return (0);
+}
+
+static st_int	check_input_validity(int argc, char **argv)
+{
+	if (argc != 2)
+		return (-1);
+	//TODO: check the string argv[1] ...
 }
--- rush01/rush01.h
+++ rush01/rush01.h
@@ -6,34 +6,34 @@
 /*   By: Rene Ciak <rciakAT42Vienna@web.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 22:42:28 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/16 00:13:49 by Rene Ciak        ###   ########.fr       */
+/*   Updated: 2025/02/16 09:37:45 by Rene Ciak        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RUSH01_H
 # define RUSH01_H
 # define SIZE 4
-	
-//TODO: CHECK if in accordance to the pdf
-//TODO: Transform into enum if allowed by the norm	
+# define NUM_LOOKING_DIRECTIONS 4
+# define NUM_OF_BASIC_MATRICES 4
 # define LOOK_FROM_NORTH 0
 # define LOOK_FROM_SOUTH 1
 # define LOOK_FROM_WEST 2
 # define LOOK_FROM_EAST 3
 
+//TODO: CHECK if in accordance to the pdf
+//TODO: Transform into enum if allowed by the norm	
 
 
+typedef struct s_gen_perms
+{
+	char	row_perm[6][SIZE];
+	char	col_perm[24][SIZE];
+}	t_gen_perms;
 
-	typedef struct	s_gen_perms
-	{
-		char	row_perm[6][SIZE];
-		char	col_perm[24][SIZE];
-	} t_gen_perms
-
-
-	int		check_input_validity(char *arg);
-	void	set_the_matrix();      //TODO: HowTo make it fitting?
-	void	set_row_perm();        //TODO: HowTo make it fitting?
-	void	set_col_perm(col_perm);
-	//TODO: HowTo make it fitting?
+int		check_input_validity(char *arg);
+void	clear_solution(char **solution);
+void	set_the_matrix(char ***the_matrix);
+void	set_row_perm(char **row_perm);
+void	set_col_perm(char **col_perm);
+void	print_solution(char **solution);
 #endif
