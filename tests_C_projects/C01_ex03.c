3c898c9c6cba7f564acda64c366f1ef97d3d10d4
Rene Ciak <reciak@student.42vienna.com>
2025-02-20 17:50:52 +0100

    Testing: Style: More nice output in  C01_ex03.c  test file

--- tests_C_projects/C01_ex03.c
+++ tests_C_projects/C01_ex03.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 00:08:29 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/19 21:31:41 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/20 17:33:10 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,21 @@ int	main(void)
 {
 	int	x;
 	int	y;
-	int res_div;
+	int	res_div;
 	int	res_mod;
 
 	x = -1;
 	y = 4;
 	res_div = 0;
 	res_mod = 0;
-	printf("before calling ft_div_mod(x, y, &res_div, &res_mod) the var x is: %d\n", x);
-	printf("before calling ft_div_mod(x, y, &res_div, &res_mod) the var y is: %d\n", y);
-	printf("before calling ft_div_mod(x, y, &res_div, &res_mod) the var res_div is: %d\n", res_div);
-	printf("before calling ft_div_mod(x, y, &res_div, &res_mod) the var res_mod is: %d\n", res_mod);
+    printf("before calling ft_div_mod(x, y, &res_div, &res_mod) ... \n");
+	printf("  x is: %d\n", x);
+	printf("  y is: %d\n", y);
+	printf("  res_div is: %d\n", res_div);
+	printf("  res_mod is: %d\n", res_mod);
 	printf("--------------------------------------------------------------------\n");
 	ft_div_mod(x, y, &res_div, &res_mod);
-	printf("after calling ft_div_mod(x, y, &res_div, &res_mod) the var res_div is: %d\n", res_div);
-	printf("after calling ft_div_mod(x, y, &res_div, &res_mod) the var res_mod is: %d\n", res_mod);
+	printf("after calling ft_div_mod(x, y, &res_div, &res_mod) ... \n");
+	printf("  res_div is: %d\n", res_div);
+	printf("  res_mod is: %d\n", res_mod);
 }
