e47515015f9434f7f0e6a97c76c375ccdc602f0e
Rene Ciak <reciak@student.42vienna.com>
2025-02-22 00:10:23 +0100

    C03, ex03: Tested  ft_strncat

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,8 +6,26 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 23:14:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/20 23:18:47 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/22 00:08:03 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	char	*ptr;
+
+	ptr = dest;
+	while (*ptr)
+		ptr++;
+	while (*src && nb > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		nb--;
+	}
+	*ptr = '\0';
+	return (dest);
+}
 
