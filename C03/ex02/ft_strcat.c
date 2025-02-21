fbf9dd1c1167d80cbe299e7466644168331707dc
Rene Ciak <reciak@student.42vienna.com>
2025-02-21 23:19:25 +0100

    C03, ex02: Fixed  ft_strcat.c
    also norminetted

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -6,23 +6,23 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 23:25:29 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/20 23:38:15 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/21 23:19:06 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
 	char	*ptr;
 
 	ptr = dest;
-	while(*ptr)
+	while (*ptr)
 		ptr++;
-	while(*src)
+	while (*src)
 	{
 		*ptr = *src;
 		ptr++;
 		src++;
 	}
-	*dest = '\0';
+	*ptr = '\0';
 	return (dest);
 }
