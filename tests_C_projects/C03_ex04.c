69db6e2d9f9bd235cfc84fc1f6962a39d431833b
Rene Ciak <reciak@student.42vienna.com>
2025-02-22 02:29:10 +0100

    Testing: Added test for  C03, ex04  ft_strstr

---
+++ tests_C_projects/C03_ex04.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   C03_ex04.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 01:26:35 by reciak            #+#    #+#             */
+/*   Updated: 2025/02/22 02:25:46 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+char		*ft_strstr(char *str, char *to_find);
+static void	print_testreport(char *str1, char *str2, char *ori, char *own);
+
+int	main(int argc, char **argv)
+{
+	char	*haystack;
+	char	*needle;
+	char	*ori;
+	char	*own;
+
+	if (argc != 1 + 2)
+	{
+		write(2, "Arguments!\n", 11);
+		return (ARGC_WRONG);
+	}
+	haystack = argv[1];
+	needle = argv[2];
+	ori = strstr(haystack, needle);
+	own = ft_strstr(haystack, needle);
+	print_testreport(haystack, needle, ori, own);
+	if (ori == own)
+		return (NO_ERR);
+	else
+		return (TEST_FAILED);
+
+}
+
+static void print_testreport(char *str1, char* str2, char *ori, char *own)
+{
+    printf("---------------------------------------------------------------\n");
+    printf("Received: |%s|\n", str1);
+    printf("Received: |%s|\n", str2);
+    printf("strncat:      |%s|\n", ori);
+    printf("ft_strncat:   |%s|\n", own);
+    if (ori == own)
+        printf("✅\n");
+    else
+        printf("❌\n");
+}
