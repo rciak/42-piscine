d9827f86c6807c53bf5c7aeaa05eeb5da4458c34
Rene Ciak <reciak@student.42vienna.com>
2025-02-24 23:27:34 +0100

    C04, ex00: Fixed after finally I got my improved tester running
    (some refactoring trying to approach towards a more easy to adopt version

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 19:23:39 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/24 19:25:01 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/24 23:18:30 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strlen(char *str)
 	int	len;
 
 	len = 0;
-	while (*str)
+	while (str[len])
 		len++;
 	return (len);
 }
