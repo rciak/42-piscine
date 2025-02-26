7a56114bd81947a77f338f4cc23001d6cb53f2fb
Rene Ciak <reciak@student.42vienna.com>
2025-02-26 17:53:23 +0100

    C04, ex03: Complemented code by adding already coded functions
    also shortend name: candidate --> candid

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 12:38:34 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/26 17:38:55 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/26 17:52:35 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include <stddef.h>    // size_t, NULL
 #include <bool.h>      // true, false
 
+
+static int	st_ft_strcmp(char *s1, char *s2);
+static void	st_var_putstr(char *str);
 static bool	st_find_next(char **p_cur_content, int argc, char **argv);
 static bool	st_found_successor(char* cur, int argc, char **argv, size_t i);
 
@@ -28,10 +31,10 @@ int	main(int argc, char **argv)
 		i = 1;
 		while (i < argc)
 		{
-			if (ft_strcmp(cur_content, argv[i]) == 0)
+			if (st_ft_strcmp(cur_content, argv[i]) == 0)
 			{
-				ft_putstr(cur_content);
-				ft_putstr("\n");
+				st_var_putstr(cur_content);
+				st_var_putstr("\n");
 			}
 			i++;
 		}
@@ -59,18 +62,45 @@ static bool	st_find_next(char **p_cur_content, int argc, char **argv)
 static	bool st_found_successor(char* cur, int argc, char **argv, size_t i)
 {
 	size_t	k;
-	char	*candidate;
+	char	*candid;
 
-	candidate = argv[i];
-	if (ft_strcmp(cur, candidate) >= 0)
+	candid = argv[i];
+	if (st_ft_strcmp(cur, candid) >= 0)
 		return (false);
 	k = 1;
 	while (k < argc)
 	{
-		if (ft_strcmp(cur, argv[k] < 0) && ft_strcmp(argv[k], candidate) < 0)
+		if (st_ft_strcmp(cur, argv[k] < 0) && st_ft_strcmp(argv[k], candid) < 0)
 			return (false);
 		k++;
 	}
 	return (true);
 }
+
+int	st_ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+	else
+		return (0);
+}
+
+void	st_var_putstr(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+	write(1, str, len);
+}
+
 
