32b5b059cc660813cd3e9a0adf34ad532a1fdc40
Rene Ciak <reciak@student.42vienna.com>
2025-02-26 16:20:02 +0100

    C06, ex03: Drafted  ft_sort_params.c
    Implemented:         main  and  st_find_next
    Not yet implemented: Three static helper functions

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 12:38:34 by reciak            #+#    #+#             */
+/*   Updated: 2025/02/26 16:19:49 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>    // write
+#include <stddef.h>    // size_t, NULL
+#include <bool.h>      // true, false
+
+static bool		st_find_next(char **p_cur_content, int argc, char **argv);
+static			st_found_successor(char* cur_content, char **argv, size_t i);
+static	size_t	st_count_occurences(char *cur_content, int argc, char **argv);
+static	void	st_print(char *cur_content, size_t occurs);
+
+int	main(int argc, char **argv)
+{
+	char 	*cur_content;
+	size_t	occurs;
+
+	cur_content = NULL;
+	index = 0;
+	occurs = 0;
+	while (st_find_next(&cur_content, argc, argv) != NULL)
+	{
+		occurs = st_count_occurences(cur_content, argc, argv);
+		st_print(cur_content, occurs);
+	}
+}
+
+static bool		st_find_next(char **p_cur_content, int argc, char **argv)
+{
+	size_t	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (st_found_successor(*p_cur_content, argv, i))
+		{
+			*p_cur_content = argv[i];
+			return (true);
+		}
+		i++;
+	}	
+	*p_cur_content = NULL;
+	return (false);
+}
+
+static			st_found_successor(char* cur_content, char **argv, size_t i)
+{
+
+}
+
+static	size_t	st_count_occurences(char *cur_content, int argc, char **argv)
+{
+
+}
+
+static	void	st_print(char *cur_content, size_t occurs)
+{
+
+}
