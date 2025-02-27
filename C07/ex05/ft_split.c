8f9de069df20be152f314fcc3b5ff944c8482daa
Rene Ciak <reciak@student.42vienna.com>
2025-02-27 18:58:32 +0100

    C07, ex05 : Second try: drafting of ft_split done, some notes:
    Currently 27 line long; the Variable "i" (afte everything is working)
    can though be omitted, comming back to the 25 lines per function limit.
    
    Static helper functions though still needs to be implemented.

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,32 +6,23 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 12:56:25 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/27 17:59:15 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/27 18:58:07 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>    // malloc
 
-char	*ft_strdup(char *src);
-
-	bold = str;
-	care = str;
-	
-char	*bold;
-	char	*care;
-
-
-
-
-
-
+static size_t	st_count_words(char *str, char *charset);
+static size_t	st_is_sep(char *str, char* charset);
+static size_t	st_word_len(char *str, char* charset);
+static void		st_free_heap(char **array, i);
 
 char **ft_split(char *str, char *charset)
 {
-	size_t	num_words;
-	size_t	word_len;
-	char	**array;
 	size_t	i;
+	size_t	num_words;
+	char	**array;
+	size_t	word_len;
 
 	num_words = st_count_words(str, charset);
 	array = malloc((num_words + 1) * sizeof(char *));
@@ -40,15 +31,40 @@ char **ft_split(char *str, char *charset)
 	i = 0;
 	while (i < num_words)
 	{
-		word_len = st_get_wordlen(word_start, charset);
-		array[i] = malloc((wordlen + 1) * sizeof(char));
+		while (st_is_sep(str, charset))
+			str++;
+		word_len = st_word_len(str, charset);
+		array[i] = malloc((word_len + 1) * sizeof(char));
 		if (array[i] == NULL)
 		{
-			st_free_stuff(array, i);
+			st_free_heap(array, i);
 			return (NULL);
 		}
+		while (!st_is_sep(str, charset) && *str != '\0')
+			str++;
 		i++;
 	}
 	array[i] = NULL;
 	return (array);	
+}
+
+static size_t	st_count_words(char *str, char *charset)
+{
+
+}
+
+static size_t	st_is_sep(char *str, char* charset);
+{
+
+}
+
+
+static size_t	st_word_len(char *str, char* charset)
+{
+
+}
+
+static void		st_free_heap(char **array, i)
+{
+
 }
