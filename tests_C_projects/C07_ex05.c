58fcd2bfb612ea3585f30903f0f9deac0c70bb57
Rene Ciak <reciak@student.42vienna.com>
2025-02-27 21:29:30 +0100

    Testing: Added newline for better readability of test results

--- tests_C_projects/C07_ex05.c
+++ tests_C_projects/C07_ex05.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 20:53:26 by reciak            #+#    #+#             */
-/*   Updated: 2025/02/27 21:26:18 by reciak           ###   ########.fr       */
+/*   Updated: 2025/02/27 21:29:20 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ int main (int argc, char **argv)
 		fprintf(stderr, "Error: Wrong number of arguments (2 needed)\n");
 		return (1);
 	}
-	printf("1. received: |%s|", argv[1]);
-	printf("2. received: |%s|", argv[2]);
+	printf("1. received: |%s|\n", argv[1]);
+	printf("2. received: |%s|\n", argv[2]);
 	arr = ft_split(argv[1], argv[2]);
 	i = 0;
 	while (arr[i] != NULL)
 	{
-		printf("arr[%d] : |%s|", i, arr[i]);
+		printf("arr[%d] : |%s|\n", i, arr[i]);
 		i++;
 	}
 	return (0);
